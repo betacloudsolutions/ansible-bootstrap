bb7d504937c2e78a5ad2fde9662a90b2b4b6ce30
Christian Berendt <berendt@b1-systems.de>
2016-11-29 13:59:25 +0100

    Import betacloud.bootstrap from betacloud-ansible repository

---
+++ README.md
@@ -0,0 +1,31 @@
+# Ansible betacloud.docker
+
+This is an Ansible role that installs Docker on Ubuntu servers.
+
+Requirements
+------------
+
+Ubuntu 16.04 (Xenial)
+
+Dependencies
+------------
+
+none
+
+Example Playbook
+----------------
+
+```yml
+- hosts: all
+
+  roles:
+  - role: betacloud.docker
+
+```
+
+Changelog
+---------
+
+**TODO**
+
+**20161129** (first release)
---
+++ scripts/push.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+USERNAME=betacloud-ansible
+PASSWORD=hie0Wof2the0uGeu
+
+NAME=betacloud.bootstrap
+TAG=$(date +%Y%m%d)
+ARCHIVE=$NAME-$TAG.tar.gz
+
+LAST_TAG=$(git describe --abbrev=0 --tags)
+if [[ $(git diff master..$LAST_TAG) ]]; then
+    git tag $TAG
+    git push --tags
+    tar cvzf $ARCHIVE README.md defaults files handlers meta tasks
+    mkdir $NAME
+    tar xvzf $ARCHIVE -C $NAME
+    tar cvzf $ARCHIVE $NAME
+    curl -v --user "$USERNAME:$PASSWORD" --upload-file $ARCHIVE https://nexus.betacloud.io/repository/ansible/$ARCHIVE
+    rm -f $ARCHIVE
+    rm -rf $NAME
+else
+    echo "no change since latest tag $LAST_TAG"
+fi
